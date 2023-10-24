6e50c1a1169ef41b1f65aa60454cd952d657fcb9
Quan Cao <quancao2310@gmail.com>
2023-10-24 23:02:10 +0700

    redefine some functions and test libraries

--- client.py
+++ client.py
@@ -1,13 +1,11 @@
 import socket
 import threading
 from ftplib import FTP, FTP_TLS
-from pyftpdlib import server
-
-# To be changed in the future (very soon), please do not rely heavily on this blueprint.
-# Discuss more with the author.
-# C'est mon anniversaire aujourd'hui.
-# 今日は私の誕生日です。
-# Kyō wa watashi no tanjōbidesu.
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import ThreadedFTPServer
+import logging
+import time
 
 class Client:
     def __init__(self, server_host, server_port):
@@ -15,45 +13,62 @@ class Client:
         Constructor: Initializes attributes
         To be added more (I don't know)
         """
+        # Initialize server information
         self.server_host = server_host
         self.server_port = server_port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.published_files = []
 
-    # def __join_network__(self):
-    #     try:
-    #         pass
-    #     except:
-    #         pass
+        # Initialize FTP server
+        authorizer = DummyAuthorizer()
+        authorizer.add_user('user', '54321', '.', perm='rl')
+        authorizer.add_anonymous('.', perm='rl')
+        handler = FTPHandler
+        handler.authorizer = authorizer
+        handler.banner = "Connection Success"
+        # handler.masquerade_address = '171.227.71.146' # external IP address
+        # handler.passive_ports = range(60000, 65535)
+
+        self.server = ThreadedFTPServer(('0.0.0.0', 21), handler)
+        self.server.max_cons = 256
+        self.server.max_cons_per_ip = 5
+
+        logging.basicConfig(filename='tmp.log', level=logging.INFO) # log file
     
     
-    
-    def connectServer(self):
+    def connect(self):
         """
-        Establish a TCP connection to the centralized server
+        Register server to join the P2P network
+
+        Return: None
         """
         pass
-    
-    def __requestServer__(self, fname):
+
+    def disconnect(self):
         """
-        Request from the server the list of hosts having the given file fname
+        Disconnect from the P2P network
+        """
+
+    def ftp_server(self):
+        """
+        Allocate and establish the server for FTP connection
+
+        Return: None
+        """
+        pass
+
+    def ftp_client(self, host='localhost'):
+        """
+        Establish an FTP connection to the host server
 
         Parameters:
-        - fname: The file to be requested from the server
+        - host: client's IP address to connect
 
-        Returns: (List) A list of hosts possessing the file fname
+        Return: None
         """
-        pass
-    
-    def connectClient(self):
-        """
-        """
-        pass
-    
-    def transferFile(self):
         pass
 
-    def __upload_file__(self, lname, fname):
+    def publish(self, lname, fname):
         """
         A local file (which is stored in the client's file system at lname) is added to the client's repository as a file named fname and this information is conveyed to the server.
 
@@ -65,7 +80,7 @@ class Client:
         """
         pass
 
-    def __download_file__(self, fname):
+    def fetch(self, fname):
         """
         fetch some copy of the target file and add it to the local repository
 
@@ -75,22 +90,57 @@ class Client:
         Return: None
         """
         pass
+        
+    # Dự phòng
+    def __request_server__(self, fname):
+        """
+        Request from the centralized server the list of hosts having the given file fname
 
-    def publish(self, lname, fname):
-        return self.__upload_file__(lname, fname)
+        Parameters:
+        - fname: The file to be requested from the server
 
-    def fetch(self, fname):
-        return self.__download_file__(fname)
+        Returns: (List) A list of hosts possessing the file fname
+        """
+        pass
+    
+    def __transfer_file__(self):
+        """
+        To be used to transfer file, not yet defined
+        """
+        pass
 
-    def __testFTPClient(self):
-        ftp = FTP('ftp.us.debian.org')  # connect to host, default port
-        ftp.login()                     # user anonymous, passwd anonymous@
-        # ftp.cwd('debian')               # change into "debian" directory
-        ftp.retrlines('LIST')           # list directory contents
+    # These are for testing purposes of the libraries, do not use them
+    def testFTPClient(self, host='localhost', dir='.', fname='test.txt'):
+        ftp = FTP(host)  # connect to host, default port
+        ftp.login()
+        filepath = dir + '/' + fname
+        
         with open('README', 'wb') as fp:
-            ftp.retrbinary('RETR /debian/doc/bug-log-access.txt', fp.write)
+            ftp.retrbinary(f'RETR {filepath}', fp.write)
+        
         ftp.quit()
 
+    def FTP_serve__(self):
+        thread1 = threading.Thread(target=self.server.serve_forever)
+        thread1.start()
+        # time.sleep(5)
+        # print(1)
 
-obj = Client('localhost', 12345)
-obj.connectClient()
+    def FTP_shutdown__(self):
+        self.server.close_all()
+
+def main():
+    obj = Client('localhost', 12345)
+    while True:
+        tmp = input('Choose opcode: ')
+        if tmp == '1':
+            obj.FTP_serve__()
+        elif tmp == '2':
+            obj.FTP_shutdown__()
+        elif tmp == '3':
+            obj.testFTPClient()
+        else:
+            break
+
+if __name__ == '__main__':
+    main()

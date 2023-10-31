f5920f3f2b99754d5310a1ad1f0a6b926b895b93
Quan Cao <quancao2310@gmail.com>
2023-11-01 02:07:33 +0700

    file transfer functionality enhanced

--- message.py
+++ message.py
@@ -1,7 +1,9 @@
 from enum import Enum
+from json import loads
+
 class Type(Enum):
-    SEND = 0
-    RECEIVE = 1
+    REQUEST = 0
+    RESPONSE = 1
 
 class Header(Enum):
     PING = 0
@@ -11,9 +13,17 @@ class Header(Enum):
     TAKE_HOST_LIST = 4
     RETRIEVE_REQUEST = 5
     RETRIEVE_PROCEED = 6
+    LOG_IN = 7
+    LOG_OUT = 8
+    END_CONNECTION = 9
 
 class Message:
-    def __init__(self, header, type, info):
+    def __init__(self, header, type, info, json_string=None):
+        if json_string:
+            __tmp__ = loads(json_string)
+            header = Header(__tmp__['header'])
+            type = Type(__tmp__['type'])
+            info = __tmp__['info']
         self._header = header
         self._type = type
         self._info = info
@@ -28,4 +38,4 @@ class Message:
         return self._info
     
     def get_packet(self):
-        return self.__dict__
+        return {'header': self._header.value, 'type': self._type.value, 'info': self._info}
